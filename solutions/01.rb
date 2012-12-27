cd5df19fe842328b6de0f5c8b51229eda43c686f
Nelly Hateva <nelly.hateva@gmail.com>
2012-12-27 17:17:16 +0200

    Update solutions/01.rb

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,73 +1,33 @@
 class Integer
-
   def prime?
-    possible = (2..self-1)
-    
-    possible.each do |d|
-      return false if self % d == 0
-    end
-    
-    true
+    2.upto(Math.sqrt(self)).all? { |number| (self % number).nonzero? }
   end
 
   def prime_divisors
-    num = self.abs
-    return [num] if num.prime?
-    result = []
-    possible = (2..num-1)
-
-    possible.each do |d|
-      result << d if d.prime? and num % d == 0
-    end
-
-    return result
+    2.upto(abs).select { |divisor| (abs % divisor).zero? and divisor.prime?}
   end
-
 end
 
-
 class Range
-
   def fizzbuzz
-    result = []
-
-    self.each do |number|
-      if number % 3 == 0 && number % 5 == 0
-        result << :fizzbuzz
-      elsif number % 3 == 0 
-        result << :fizz
-      elsif number % 5 == 0 
-        result << :buzz
-      else 
-        result << number
+    map do |number|
+      if number % 15 == 0 then :fizzbuzz
+      elsif number % 3 == 0 then :fizz
+      elsif number % 5 == 0 then :buzz
+      else number
       end
     end
-
-    result
   end
-
 end
 
 class Hash
-
   def group_values
-    result = Hash.new { |h,k| h[k] = (self.select { |key,value| value == k }).keys}
-
-    self.each_value { |value| result[value]}
-
-    result
+    keys.group_by { |key| self[key] }
   end
-
 end
 
 class Array
-
   def densities
-    result = []
-
-    self.each { |elem| result << self.count(elem) }
-
-    result
+    map { |element| count element }
   end
-
 end
